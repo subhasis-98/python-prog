d7c0f9becf4963caae031e3c57a9cbeaf48f802a
SubhasisMunu <Subhasissahoo9040@gmail.com>
2024-10-22 00:51:47 +0530

    string menthods

---
+++ string-methods.py
@@ -0,0 +1,18 @@
+str= "hey Jarvis!!!!!!!!!"     #strings are immutable in python
+print(len(str))
+
+print(str.upper())
+
+print(str.lower())
+
+print(str.capitalize())
+
+print(str.rstrip("!"))
+
+print(str.replace("Jarvis","ultron"))
+
+print(str.split(" "))
+
+print(str.center(100))
+
+print(str.count("!"))
