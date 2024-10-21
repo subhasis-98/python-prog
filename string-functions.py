71fef93493784d7d118e85cf1553f6676a86b3da
SubhasisMunu <Subhasissahoo9040@gmail.com>
2024-10-22 00:38:34 +0530

    strings

---
+++ string-functions.py
@@ -0,0 +1,18 @@
+name = "subhasis"
+
+# string index number
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+print(name[7])
+
+# string slicing
+name = "subhasis,munu"
+print(name[0:8])
+print(name[9:13])
+print(len(name))     #to get string length
+
---
+++ string-prog.py
@@ -0,0 +1,15 @@
+name = "subhasis"
+print(name)
+print("hello "+name)
+message = 'hey i am "subhasis"'    #to add quatation without using \"
+print("\n\n\n"+message)
+
+
+annmessage = '''Python is a high-level, interpreted programming language known 
+for its simplicity and readability. It was created by Guido van Rossum and 
+released in 1991. Python is widely used for web development, data analysis, 
+artificial intelligence, scientific computing, automation, and more.'''
+
+# by using ''' text''' we can write multiple line string
+
+print("\n\n\n"+annmessage)
